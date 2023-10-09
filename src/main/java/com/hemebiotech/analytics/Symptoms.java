56e2bc80359ca8d5f30463ced45edb5ad2623aaa
IzawGithub <125356732+IzawGithub@users.noreply.github.com>
2023-10-09 15:54:59 +0200

    Add `Symptoms.java` ctor

--- src/main/java/com/hemebiotech/analytics/Symptoms.java
+++ src/main/java/com/hemebiotech/analytics/Symptoms.java
@@ -1,5 +1,12 @@
 package com.hemebiotech.analytics;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.jetbrains.annotations.NotNull;
+
 /**
  * Read a file and extract a list of symptoms from it.
  * <p>
@@ -17,4 +24,28 @@ class Symptoms {
 
     // public
 
+
+    // -- Ctors --
+
+    /**
+     * Create a Symptoms object.
+     * Read from a file given from a path, and extract a list of symptoms from the
+     * data.
+     * Duplicate symptoms in the file increment the value.
+     * 
+     * @param pathToFile Path to a text file on the filesystem
+     * @throws IOException Thrown if the file is either null or does not exist
+     */
+    public @NotNull Symptoms(@NotNull final String pathToFile) throws IOException {
+        if (pathToFile == null) {
+            throw new IOException("Path to symptoms file cannot be null");
+        }
+
+        var file = new File(pathToFile);
+        if (!file.exists() || file.isDirectory() || !file.canRead()) {
+            throw new IOException("Path given does not exist, or is not a file");
+        }
+        var fileReader = new BufferedReader(new FileReader(file));
+        // BufferedReader implements java.lang.AutoCloseable, does not leak
+    }
 }
