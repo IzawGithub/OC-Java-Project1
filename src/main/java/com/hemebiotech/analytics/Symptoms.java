d759b18d7e16e20d84c0d07095639a9e85462c03
IzawGithub <125356732+IzawGithub@users.noreply.github.com>
2023-10-09 15:52:04 +0200

    Add Symptoms class declaration

---
+++ src/main/java/com/hemebiotech/analytics/Symptoms.java
@@ -0,0 +1,20 @@
+package com.hemebiotech.analytics;
+
+/**
+ * Read a file and extract a list of symptoms from it.
+ * <p>
+ * The Symptoms class algorithm work by extracting text line by line, and
+ * counting the number of repeat.
+ * <p>
+ * There is no special word filter to see if a line is actually a symptom.
+ * This also mean that empty line count as a symptom.
+ * 
+ */
+class Symptoms {
+    // private
+
+    // protected
+
+    // public
+
+}
